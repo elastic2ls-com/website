255e4da47b6844a9126d36128c37c25a4246632b
AlexanderWiechert <alexander.wiechert@bundesliga.de>
2019-11-26 13:17:25 +0100

    added test folder

---
+++ test/get.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#redirect source
+getsrc=$(grep RewriteRule redirects| awk {'print $2'}|cut -d / -f1,2,3 |cut -d ^ -f2|sed 's/.\{1\}$//')
+
+#redirect target
+gettrg=$(grep RewriteRule redirects| awk {'print $3'}|cut -d / -f4,5 )
+
+for redirects in $getsrc
+        do
+                echo $redirects
+                curl -Is www.elastic2ls.com/$redirects
+        done
